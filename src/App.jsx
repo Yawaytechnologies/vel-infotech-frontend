0f79d2f97c09a650d9588de1e8342b95fa921683
Sowjanya-S <sowjanya503@yawaytech.com>
2025-07-22 11:09:57 +0530

    base project and neccessary packages installed

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <div class="bg-background text-green-800 border-l-4 text-bold border-green-500 mt-50 text-center p-2 rounded">
+        ✅ Success: I am Sridhar. created by sowjanya
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/redux/store/store.js
@@ -0,0 +1,9 @@
+// src/redux/store/store.js
+import { configureStore } from '@reduxjs/toolkit';
+
+
+export const store = configureStore({
+  reducer: {
+    
+  },
+});
