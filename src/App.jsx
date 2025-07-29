089b1a41ba25b82d5a9e651d78f4be05c687ff9e
sathish-dev <sathish@yawaytech.com>
2025-07-29 15:06:47 +0530

    SCRUM-8-fe-admin-page

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,41 @@
-import { useState } from "react";
-import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
-
-
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import React from "react";
 import Home from "./pages/Home";
 import Header from "./components/common/header";
 import PlacedStudents from "./pages/placedStudents";
-
-import MultiRowHeader from "../src/components/common/MultiHeader";
-
 import Footer from "./components/common/Footer";
-function App() {
+import AdminDashboard from "../src/pages/Admin";
 
+// Wrapper component to use useLocation
+function Layout({ children }) {
+  const location = useLocation();
+  // List all admin paths here
+  const adminPaths = ["/admin/login", "/admin/dashboard"];
+  const isAdmin = adminPaths.includes(location.pathname);
 
   return (
-    <Router>
-    <div className="bg-white">
-    
-    <Header/>
-
-    <Routes>
-      <Route path="/" element={<Home />} />
-       <Route path="/placed-students" element={<PlacedStudents />} />
-       
-    </Routes>
-
-<Footer />
-    
-      
+    <div className="bg-white min-h-screen flex flex-col">
+      {!isAdmin && <Header />}
+      <div className="flex-1">{children}</div>
+      {!isAdmin && <Footer />}
     </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          {/* User routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/placed-students" element={<PlacedStudents />} />
+
+          {/* Admin routes */}
+          {/* <Route path="/admin/login" element={<AdminLogin />} /> */}
+          <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 }
