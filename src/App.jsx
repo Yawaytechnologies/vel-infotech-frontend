c7f23598e8086af4f8b0224610425e1dcfa10243
kanaka@yawaytech.com <kanaka504@yawaytech.com>
2025-08-18 17:35:07 +0530

    Merge branch 'develop' of https://github.com/Yawaytechnologies/vel-infotech-frontend into feature/SCRUM-60-review-sample-resume-page

--- src/App.jsx
+++ src/App.jsx
@@ -54,6 +54,7 @@ import DigitalMarketingPage from "./components/Courses/DigitalMarketing";
 import SoftSkillPage from "./components/Courses/SoftSkillsTraining";
 
 import Reviews from "./pages/Reviews";
+
 import SampleResume from "./pages/SampleResume";
 
 // Wrapper component to use useLocation
